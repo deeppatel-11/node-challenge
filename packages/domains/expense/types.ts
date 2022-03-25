837a3b7a001a920f6c95a5a7f3c1b3876200509b
deeppatel-11 <deeppatel770@gmail.com>
2022-03-25 11:19:41 -0400

    Creating Expenses Interface

---
+++ packages/domains/expense/types.ts
@@ -0,0 +1,9 @@
+export interface Expenses {
+    id: string
+    merchantName: string
+    amountInCents: string
+    currency: string
+    userId: string
+    dateCreated: Date
+    status: string
+}
